0fba3edca756452b62143de06b09aa42dc618134
dehu robin <rdehu@prune>
2014-11-14 16:41:47 +0100

    7e test tag échec

--- test/test.js
+++ test/test.js
@@ -29,7 +29,7 @@ MonTestCase.prototype.test_retire_deux_pions=function(){
     assertTrue(p.get_nb_player_color(1,0)===2);
 };
 
-MonTestCase.prototype.test_partie_complete=function(){
+MonTestCase.prototype.test_partie_complete_1=function(){
     p.new_board();
     p.new_game();
     p.get_marble("A1");
@@ -73,6 +73,68 @@ MonTestCase.prototype.test_partie_complete=function(){
 
     console.log("Player 1 noir : "+p.get_nb_player_color(0,0));
 
+
+    assertTrue(p.check_winner()===1);
+};
+
+MonTestCase.prototype.test_partie_complete_2=function(){
+    //e.new_board();
+    p.new_board();
+    p.new_game();
+    p.get_marble("A1");
+    p.get_marble("F6");
+    p.change_player();
+    p.get_marble("A2");
+    p.get_marble("A6");
+    p.change_player();
+    p.get_marble("A5");
+    p.get_marble("F6");
+    p.change_player();
+    p.get_marble("E6");
+    p.get_marble("B1");
+    p.get_marble("F5");
+    p.change_player();
+    p.get_marble("A3");
+    p.get_marble("F2");
+    p.change_player();
+    p.get_marble("C1");
+    p.get_marble("F4");
+    p.change_player();
+    p.get_marble("E1");
+    p.get_marble("D6");
+    p.get_marble("A4");
+    p.get_marble("F3");
+    p.change_player();
+    p.get_marble("D1");
+    p.get_marble("B6");
+    p.change_player();
+    p.get_marble("E2");
+    p.get_marble("E5");
+    p.change_player();
+    p.get_marble("C6");
+    p.get_marble("D5");
+    p.get_marble("E3");
+    p.change_player();
+    p.get_marble("B5");
+    p.change_player();
+    p.get_marble("D2");
+    p.change_player();
+    p.get_marble("B2");
+    p.get_marble("D3");
+    p.change_player();
+    p.get_marble("E4");
+    p.change_player();
+    p.get_marble("C5");
+    p.get_marble("B3");
+    p.change_player();
+    p.get_marble("D4");
+    p.get_marble("C2");
+    p.change_player();
+    p.get_marble("C3");
+    p.change_player();
+    p.get_marble("B4");
+    p.change_player();
+    p.get_marble("C4");
 
     assertTrue(p.check_winner()===1);
 };
